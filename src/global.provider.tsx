3bc5e23d23a54e254a851d9ff27f29beedcfd377
fabio <fabio@fullin.com.ar>
2025-10-03 09:18:57 -0300

    Fixes de tipos de datos de ts

--- src/global.provider.tsx
+++ src/global.provider.tsx
@@ -1,11 +1,11 @@
-import { useState, type ReactNode } from "react"
+import { useState, type ReactElement, type ReactNode } from "react"
 import { GlobalContext, EmptyGlobalState, type GlobalContextValueType } from "./global.context"
 
 interface GlobalProps {
     children: ReactNode
 }
 
-export const GlobalProvider = ({children}: GlobalProps) => {
+export const GlobalProvider = ({children}: GlobalProps): ReactElement => {
     const [value, setValue] = useState<GlobalContextValueType>(EmptyGlobalState)
 
     return (
