62a0381745bfd28bdec6dae69ec2e827400fc110
fabio <fabio@fullin.com.ar>
2025-10-11 22:20:36 -0300

    Prueba2UseEffect fetch de avengers

---
+++ src/components/Prueba2UseEffect.tsx
@@ -0,0 +1,49 @@
+import { useGeneralFetch } from '../hooks/useGeneralFetch';
+
+const Prueba2UseEffect = () => {
+
+    type Peliculas = 
+    {   
+        "Search": [
+            {
+            "Title": string,
+            "Year": string,
+            "imdbID": string,
+            "Type": string,
+            "Poster": string
+            }
+    ]
+        
+    }
+
+    
+    const { data } = useGeneralFetch<Peliculas>('https://www.omdbapi.com/?i=tt3896198&apikey=d42d43a3&S=avengers');
+
+  return (
+    <>
+    <div>PruebaSuperFetch</div>
+
+    {
+        data?.Search?.map(
+            (pelicula) => 
+            (
+                <div key={pelicula.imdbID}>
+                    <div>{pelicula.Title}</div>
+                    <div>{pelicula.Year}</div>
+                    <div>{pelicula.Type}</div>
+                    <img style={{width: '200px'}} src={pelicula.Poster} alt={pelicula.Title} />
+                </div>
+            )
+        )
+    }
+
+
+
+
+
+    </>
+    )
+}
+    
+
+export default Prueba2UseEffect;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import PruebaBoton from './components/PruebaBoton'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Prueba2UseEffect from './components/Prueba2UseEffect'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/boton',
     element: <PruebaBoton />
+  },
+  {
+    path: '/prueba2useeffect',
+    element: <Prueba2UseEffect />
   }
 ])
 
